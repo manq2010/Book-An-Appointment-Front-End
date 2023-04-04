6845de2117d5a43042662ffa2b19de6245fca104
cluab <cluab11222@gmail.com>
2023-04-04 11:10:31 +0200

    Add custom PrivateRoute

---
+++ src/features/Routes/PrivateRoute.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Navigate, useLocation } from 'react-router-dom';
+
+function PrivateRoute({ children }) {
+  const loading = useSelector((state) => state.session.loading);
+  const accessToken = useSelector((state) => state.session.accessToken);
+  const location = useLocation();
+  const fromLocation = (location.state)?.from;
+  const previousLocation = fromLocation || { pathname: '/login' };
+
+  if (accessToken) {
+    return children;
+  } if (loading) {
+    return <p>Loading...</p>;
+  } if (!accessToken && !loading) {
+    return <Navigate to={previousLocation} state={{ from: location }} replace />;
+  }
+  return <p>Something went wrong</p>;
+}
+
+PrivateRoute.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+export default PrivateRoute;

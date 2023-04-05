be512436873ce2ae3d4c2cd8588565962ee3694a
cluab <cluab11222@gmail.com>
2023-04-05 15:34:46 +0200

    add redux action to logout page

--- src/pages/LogoutPage.jsx
+++ src/pages/LogoutPage.jsx
@@ -1,17 +1,17 @@
 import React, { useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-// import { logoutUser } from './sessionSlice';
+import { logoutUser } from '../features/Sessions/sessionSlice';
 
 function LogoutPage() {
   const navigate = useNavigate();
-  //   const dispatch = useDispatch();
-  //   const refreshToken = useSelector((state) => state.session.accessToken);
+  const dispatch = useDispatch();
+  const refreshToken = useSelector((state) => state.session.accessToken);
 
   useEffect(() => {
-    // if (refreshToken){
-    //   dispatch(logoutUser(refreshToken));
-    // }
+    if (refreshToken) {
+      dispatch(logoutUser(refreshToken));
+    }
     navigate('/login');
   }, []);
 

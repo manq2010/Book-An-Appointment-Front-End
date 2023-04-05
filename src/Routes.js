87ab0e99eb9bda6b1aa96cba7a7506f64677c95d
cluab <cluab11222@gmail.com>
2023-04-05 16:57:14 +0200

    Config AdminOnlyRoute in Routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -18,6 +18,7 @@ import LogoutPage from './pages/LogoutPage';
 import PersistLogin from './features/Sessions/PersistLogin/PersistLogin';
 import PrivateRoute from './features/Routes/PrivateRoute';
 import PublicOnlyRoute from './features/Routes/PublicOnlyRoute';
+import AdminOnlyRoute from './features/Routes/AdminOnlyRoute';
 
 const AppRoutes = () => (
   <Routes>
@@ -27,8 +28,8 @@ const AppRoutes = () => (
         <Route exact path="/details/:symbol" element={<PrivateRoute><DetailsPage /></PrivateRoute>} />
         <Route exact path="/reservation" element={<PrivateRoute><ReservationPage /></PrivateRoute>} />
         <Route exact path="/reservations" element={<PrivateRoute><ReservationsPage /></PrivateRoute>} />
-        <Route exact path="/add-classes" element={<PrivateRoute><AddClassesPage /></PrivateRoute>} />
-        <Route exact path="/remove-classes" element={<PrivateRoute><RemoveClassesPage /></PrivateRoute>} />
+        <Route exact path="/add-classes" element={<AdminOnlyRoute><AddClassesPage /></AdminOnlyRoute>} />
+        <Route exact path="/remove-classes" element={<AdminOnlyRoute><RemoveClassesPage /></AdminOnlyRoute>} />
         <Route exact path="/login" element={<PublicOnlyRoute><LoginPage /></PublicOnlyRoute>} />
         <Route exact path="/sign-up" element={<PublicOnlyRoute><SignUpPage /></PublicOnlyRoute>} />
         <Route exact path="/logout" element={<PrivateRoute><LogoutPage /></PrivateRoute>} />

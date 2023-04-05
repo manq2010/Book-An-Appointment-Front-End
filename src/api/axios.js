64608986170cd09ca21e3ceae86da0e58380a792
cluab <cluab11222@gmail.com>
2023-04-05 14:49:29 +0200

    Update axios baseURL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_BACKEND,
   headers: {
     'Content-Type': 'application/json',
     withCredentials: true,

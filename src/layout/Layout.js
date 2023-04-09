939744872cffe357e1ad0156440c0804fcdc228a
cluab <cluab11222@gmail.com>
2023-04-09 11:47:40 +0200

    Remove old navigation

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -2,12 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 // import Footer from '../components/Footer';
 import Sidebar from '../features/Sidebar/sidebar';
-import Navbar from '../features/Navbar/navbar';
 
 const Layout = ({ children }) => (
   <div id="content">
     <Sidebar />
-    <Navbar />
     {children}
     {/* <Footer /> */}
   </div>

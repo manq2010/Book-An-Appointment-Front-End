70350bcc9317f1afdc177acd9d368ffa32a9ef7e
omarbabou <obabou50@gmail.com>
2023-04-10 09:47:00 +0200

    Add delete reducer

--- src/features/configureStore.js
+++ src/features/configureStore.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import sessionReducer from './Sessions/sessionSlice';
 import addClassesReducer from './AddClasses/addClassesSlice';
+import classesReducer from './DeleteClass/deleteClass.JS';
 
 // Create Redux store:
 const store = configureStore({
   reducer: {
     session: sessionReducer,
     addClassesReducer,
+    classes: classesReducer,
   },
 });
 

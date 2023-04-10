3cfef181d4da9c070cf95ae5bdda5c849193e08c
Frehiywot Nega <fr.nega@gmail.com>
2023-04-10 16:30:15 +0300

    change to main slice

--- src/features/Main/mainSlice.js
+++ src/features/Main/mainSlice.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-param-reassign */
-// Import createSlice() from Redux toolkit:
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 // import { useSelector } from 'react-redux';
 import axios from '../../api/axios';

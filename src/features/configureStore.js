95120a5ecc6d8e0d65be04084cbadf3fa9eaa4a3
Krishna <libraryismyparadise@gmail.com>
2023-04-10 14:36:04 +0545

    Add reserved Item and reducer

--- src/features/configureStore.js
+++ src/features/configureStore.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import sessionReducer from './Sessions/sessionSlice';
 import addClassesReducer from './AddClasses/addClassesSlice';
+import reservationReducer from './Reservation/reservationSlice';
 
 // Create Redux store:
 const store = configureStore({
   reducer: {
     session: sessionReducer,
     addClassesReducer,
+    reservationReducer,
   },
 });
 

d3ae421f9b81f823b8b7d7eb17de8aabb1869508
Frehiywot Nega <fr.nega@gmail.com>
2023-04-12 00:05:17 +0300

    change to the main style

--- src/features/Main/main.js
+++ src/features/Main/main.js
@@ -4,27 +4,15 @@ import { useSelector, useDispatch } from 'react-redux';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import SlideshowWithPagination from 'react-slideshow-with-pagination';
 import { fetchClasses } from './mainSlice';
-import './styles.css';
 
 const Section = styled.section`
-
 .item-wrapper {
   display: flex;
   flex-direction: row;
-  height: 460px;
-  }
-
-.item-card {
-  height: 460px;
-  width: 200px;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: center;
-  margin: 20px;
-  background-color: #fff;
+  height: 760px;
   overflow: hidden;
-}
+  margin-top:150px;
+  }
 
 .item-photo-wrapper {
   height: 60%;
@@ -44,7 +32,7 @@ const Section = styled.section`
 
 .item-details {
   height: 50%;
-  width: 60%;
+  width: 50%;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -64,14 +52,9 @@ const Section = styled.section`
   margin-bottom: 10px;
 }
 
-.item-price {
-  font-size: 16px;
-  font-weight: bold;
-  margin-bottom: 10px;
-}  
 `;
 
-const AddClasses = () => {
+const MainClasses = () => {
   const accessToken = useSelector((state) => state.session.accessToken);
   const classItems = useSelector((state) => state.addClassesReducer.classes);
   const classesStatus = useSelector((state) => state.addClassesReducer.status);
@@ -85,55 +68,72 @@ const AddClasses = () => {
     }
   }, [classesStatus, dispatch]);
 
-  const newClassTable = (classItem) => (
-    <React.Fragment className="item-card" key={classItem.id}>
-      <div className="item-photo-wrapper">
-        <img className="item-photo" src={classItem.photo} alt="Class" />
+  const newClassTable = (classItem1, classItem2) => (
+    <React.Fragment key={`${classItem1.id}-${classItem2 ? classItem2.id : 'null'}`}>
+      <div className="item-card">
+        <div className="item-photo-wrapper">
+          <img className="item-photo" src={classItem1.photo} alt="Class" />
+        </div>
+        <div className="item-details">
+          <div className="item-name">{classItem1.name}</div>
+          <div className="item-description">{classItem1.description}</div>
+        </div>
       </div>
-      <div className="item-details">
-        <div className="item-name">{classItem.name}</div>
-        <div className="item-description">{classItem.description}</div>
-        <div className="item-price">{classItem.price}</div>
-        <div className="item-mentor">{classItem.mentor_name}</div>
-        <div className="item-duration">{classItem.duration}</div>
-      </div>
+      {classItem2 ? (
+        <div className="item-card">
+          <div className="item-photo-wrapper">
+            <img className="item-photo" src={classItem2.photo} alt="Class" />
+          </div>
+          <div className="item-details">
+            <div className="item-name">{classItem2.name}</div>
+            <div className="item-description">{classItem2.description}</div>
+          </div>
+        </div>
+      ) : (
+        <div className="item-card" />
+      )}
     </React.Fragment>
   );
+
   let content;
 
   if (classesStatus === 'succeeded') {
-    content = classItems.length > 0 ? (
-      < >
-        <SlideshowWithPagination
-          showNumbers
-          showDots
-          showArrows
-          numberOfCardsPerScreen={2}
-          showOneCardForWidthLower="sm"
-          slideshowContainerMaxWidth="100px"
-          cardWidth="100px"
-          cardHeight={100}
-        >
-          {classItems.map((classItem) => newClassTable(classItem))}
-        </SlideshowWithPagination>
-      </>
+    const classPairs = [];
+    for (let i = 0; i < classItems.length; i += 2) {
+      const classItem1 = classItems[i];
+      const classItem2 = classItems[i + 1] || null;
+      classPairs.push([classItem1, classItem2]);
+    }
+    content = classPairs.length > 0 ? (
+      <div className="item-wrapper">
+        <div style={{ position: 'relative' }}>
+          <SlideshowWithPagination
+            option={classPairs}
+            showNumbers
+            showDots
+            showArrows
+            autoplay={false}
+            cardWidth={400}
+          >
+            {classPairs.map(([classItem1, classItem2]) => newClassTable(classItem1, classItem2))}
+          </SlideshowWithPagination>
+        </div>
+      </div>
     ) : ('');
   } else if (classesStatus === 'failed') {
     content = (
-      <>
+      <div>
         <h1>classes not found</h1>
         <p>{error}</p>
-      </>
+      </div>
     );
   }
 
   return (
     <Section>
-      <div className="item-wrapper">
-        {content}
-      </div>
+      {content}
     </Section>
   );
 };
 
-export default AddClasses;
+export default MainClasses;

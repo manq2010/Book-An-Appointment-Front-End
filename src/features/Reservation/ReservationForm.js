e0ea001d2ffeffd4dc104e5625537390344ba2e7
Krishna <libraryismyparadise@gmail.com>
2023-04-11 22:58:22 +0545

    Fix ReservationForm to add reservation

--- src/features/Reservation/ReservationForm.js
+++ src/features/Reservation/ReservationForm.js
@@ -1,31 +1,33 @@
 /* eslint-disable camelcase */
 import React, { useState, useCallback } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { useDispatch, useSelector } from 'react-redux';
+import {
+  TextField,
+  Button,
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
+} from '@mui/material';
 import { addReservation } from './reservationSlice';
 
 const Section = styled.section`
   /* Styles here */
 `;
 
-const ReservationForm = ({ classId }) => {
-  const accessToken = useSelector((state) => state.session.accessToken);
+const ReservationForm = () => {
+  const dispatch = useDispatch();
   const classes = useSelector((state) => state.addClassesReducer.classes);
   const currentUser = useSelector((state) => state.session.currentUser);
+  const accessToken = useSelector((state) => state.session.accessToken);
 
   const [values, setValues] = useState({
     date: '',
     city: '',
     item_id: '',
   });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setValues({ ...values, [name]: value });
-  };
-
-  const dispatch = useDispatch();
 
   const handleSubmit = useCallback(
     async (e) => {
@@ -35,10 +37,10 @@ const ReservationForm = ({ classId }) => {
         const reservation = {
           date,
           city,
-          item_id: classId,
-          user_id: currentUser.id,
+          item_id,
           accessToken,
         };
+        console.log(reservation);
         await dispatch(addReservation(reservation));
         setValues({
           date: '',
@@ -47,72 +49,71 @@ const ReservationForm = ({ classId }) => {
         });
       }
     },
-    [values, accessToken, classId, currentUser.id, dispatch],
+    [values, dispatch, accessToken],
   );
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues((prevState) => ({ ...prevState, [name]: value }));
+  };
 
   return (
     <Section>
       <form onSubmit={handleSubmit}>
-        <div>
-          <input
-            type="text"
-            className="form-control"
-            name="username"
-            value={currentUser.username}
-          />
-        </div>
-        <div className="form-group">
-          {/* <label htmlFor="date">Date:</label> */}
-          <input
-            type="date"
-            className="form-control"
-            name="date"
-            value={values.date}
-            required
-            onChange={handleChange}
-            id="date"
-          />
-        </div>
-        <div className="form-group">
-          {/* <label htmlFor="city">City:</label> */}
-          <input
-            type="text"
-            className="form-control"
-            name="city"
-            value={values.city}
-            required
-            onChange={handleChange}
-            id="city"
-          />
-        </div>
-        <div className="form-group">
-          {/* <label htmlFor="item_id">Select a class:</label> */}
-          <select
-            className="form-control"
+        <TextField
+          label="Username"
+          value={currentUser.username}
+          InputProps={{ readOnly: true }}
+          margin="normal"
+          fullWidth
+        />
+        <TextField
+          label="Date"
+          type="date"
+          name="date"
+          value={values.date}
+          onChange={handleChange}
+          margin="normal"
+          fullWidth
+          required
+        />
+        <TextField
+          label="City"
+          type="text"
+          name="city"
+          value={values.city}
+          onChange={handleChange}
+          margin="normal"
+          fullWidth
+          required
+        />
+        <FormControl fullWidth margin="normal" required>
+          <InputLabel id="class-label">Select a class</InputLabel>
+          <Select
+            labelId="class-label"
             name="item_id"
             value={values.item_id}
             onChange={handleChange}
-            id="item_id"
-            required
           >
-            <option value="">Select a class</option>
+            <MenuItem value="">Select a class</MenuItem>
             {classes.map((classItem) => (
-              <option key={classItem.id} value={classItem.id}>
+              <MenuItem key={classItem.id} value={classItem.id}>
                 {classItem.name}
-              </option>
+              </MenuItem>
             ))}
-          </select>
-        </div>
-        <button className="btn btn-primary" type="submit">
+          </Select>
+        </FormControl>
+        <Button variant="contained" color="primary" type="submit">
           Reserve
-        </button>
+        </Button>
       </form>
     </Section>
   );
 };
 
-ReservationForm.propTypes = {
-  classId: PropTypes.number.isRequired,
-};
+// ReservationForm.propTypes = {
+//   // eslint-disable-next-line react/require-default-props, react/no-typos
+//   classId: PropTypes.number.Optional,
+// };
 
 export default ReservationForm;

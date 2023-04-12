459c2da4cf0b39e1132c40fb4fc9bd53f45e70e6
omarbabou <obabou50@gmail.com>
2023-04-12 10:39:19 +0200

    Add DeleteSlice to the deleteItem component

--- src/features/configureStore.js
+++ src/features/configureStore.js
@@ -1,14 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import sessionReducer from './Sessions/sessionSlice';
 import addClassesReducer from './AddClasses/addClassesSlice';
-import classesReducer from './DeleteClass/deleteClass.JS';
 
 // Create Redux store:
 const store = configureStore({
   reducer: {
     session: sessionReducer,
     addClassesReducer,
-    classes: classesReducer,
   },
 });
 
--- src/pages/RemoveClassesPage.js
+++ src/pages/RemoveClassesPage.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import DeleteItem from '../features/DeleteClass/DeleteItem';
 import Layout from '../layout/Layout';
 
 const RemoveClassesPage = () => (
   <Layout>
     <main>
-      <h1>RemoveClassesPage</h1>
+      <DeleteItem />
     </main>
   </Layout>
 );

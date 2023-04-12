0449ccbe55d155df02cd54f99e32f5dacbc06585
Frehiywot Nega <fr.nega@gmail.com>
2023-04-12 16:46:40 +0300

    added tests for the fullfiled element also

--- src/__test__/addClassesSlice.test.js
+++ src/__test__/addClassesSlice.test.js
@@ -1,4 +1,6 @@
-import addClassReducer, { initialState, addClass, deleteClass } from '../features/AddClasses/addClassesSlice';
+import addClassReducer, {
+  initialState, addClass, deleteClass, fetchClasses,
+} from '../features/AddClasses/addClassesSlice';
 
 describe('addClassSlice', () => {
   describe('reducer', () => {
@@ -70,6 +72,59 @@ describe('deleteClass cases', () => {
     const action = { type: deleteClass.rejected.type, error: { message: '' } };
     const state = { ...initialState, status: 'failed' };
     const expectedState = { ...initialState, status: 'failed', error: '' };
+    expect(addClassReducer(state, action)).toEqual(expectedState);
+  });
+});
+
+describe('fetchClasses cases', () => {
+  it('should handle fetchClasses.pending', () => {
+    const action = { type: fetchClasses.pending.type };
+    const state = { ...initialState, isLoading: false, status: 'idle' };
+    const expectedState = { ...initialState, isLoading: true, status: 'loading' };
+    expect(addClassReducer(state, action)).toEqual(expectedState);
+  });
+
+  it('should handle fetchClasses.fulfilled', () => {
+    const classList = [
+      {
+        id: '1',
+        name: 'Math',
+        description: 'Learn Math',
+        photo: 'math.png',
+        price: 100,
+        mentorName: 'John Doe',
+        duration: 60,
+      },
+      {
+        id: '2',
+        name: 'Science',
+        description: 'Learn Science',
+        photo: 'science.png',
+        price: 200,
+        mentorName: 'Jane Smith',
+        duration: 90,
+      },
+    ];
+    const action = { type: fetchClasses.fulfilled.type, payload: classList };
+    const state = {
+      ...initialState, isLoading: true, status: 'loading', classes: [], content: [],
+    };
+    const expectedState = {
+      ...initialState,
+      isLoading: false,
+      status: 'succeeded',
+      classes: classList,
+      content: classList,
+    };
+    expect(addClassReducer(state, action)).toEqual(expectedState);
+  });
+
+  it('should handle fetchClasses.rejected', () => {
+    const action = { type: fetchClasses.rejected.type, error: { message: 'Error' } };
+    const state = { ...initialState, isLoading: true, status: 'loading' };
+    const expectedState = {
+      ...initialState, isLoading: false, status: 'failed', error: 'Error',
+    };
     expect(addClassReducer(state, action)).toEqual(expectedState);
   });
 });
